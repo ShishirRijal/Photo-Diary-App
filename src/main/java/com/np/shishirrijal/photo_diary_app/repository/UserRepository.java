d310897b14954906110d3834609fa5e0a205aaf4
Shishir Rijal <ccrrizal0892@gmail.com>
2025-04-29 22:55:35 +0545

    add user repository

---
+++ src/main/java/com/np/shishirrijal/photo_diary_app/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.np.shishirrijal.photo_diary_app.repository;
+
+import com.np.shishirrijal.photo_diary_app.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
