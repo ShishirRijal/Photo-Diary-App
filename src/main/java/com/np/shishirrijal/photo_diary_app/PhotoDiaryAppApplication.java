813b27aee1c4e0c9acdaae7c5bcc2444c69975b3
Shishir Rijal <ccrrizal0892@gmail.com>
2025-04-29 22:44:27 +0545

    init

---
+++ src/main/java/com/np/shishirrijal/photo_diary_app/PhotoDiaryAppApplication.java
@@ -0,0 +1,13 @@
+package com.np.shishirrijal.photo_diary_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoDiaryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoDiaryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/np/shishirrijal/photo_diary_app/PhotoDiaryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.np.shishirrijal.photo_diary_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoDiaryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
