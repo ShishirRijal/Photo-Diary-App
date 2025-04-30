7ab0045515419a8923743a7dc962bc084b9c0961
Shishir Rijal <ccrrizal0892@gmail.com>
2025-04-30 22:30:12 +0545

    setup cors

--- src/main/java/com/np/shishirrijal/photo_diary_app/config/WebConfig.java
+++ src/main/java/com/np/shishirrijal/photo_diary_app/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.np.shishirrijal.photo_diary_app.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:uploads/");
+    }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Allow CORS only for API endpoints
+                .allowedOrigins("*") // Allow all origins (no restrictions)
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(false); // Do not allow credentials (cookies, etc.)
     }
 }
