c7b90386abf7581e9a7c159dca5a012449f84a08
Shishir Rijal <ccrrizal0892@gmail.com>
2025-04-30 10:12:35 +0545

    update entities

--- src/main/java/com/np/shishirrijal/photo_diary_app/model/DiaryEntry.java
+++ src/main/java/com/np/shishirrijal/photo_diary_app/model/DiaryEntry.java
@@ -10,9 +10,6 @@ import java.util.UUID;
 
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class DiaryEntry {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -29,4 +26,82 @@ public class DiaryEntry {
     private String tags;
 
     private String createdAt;
+
+    public DiaryEntry() {
+    }
+
+    public DiaryEntry(UUID id, User user, String imageUrl, String description, String mood, String visibility, String createdAt, String tags) {
+        this.id = id;
+        this.user = user;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.mood = mood;
+        this.visibility = visibility;
+        this.createdAt = createdAt;
+        this.tags = tags;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMood() {
+        return mood;
+    }
+
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
+
+    public String getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(String visibility) {
+        this.visibility = visibility;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
 }
--- src/main/java/com/np/shishirrijal/photo_diary_app/model/User.java
+++ src/main/java/com/np/shishirrijal/photo_diary_app/model/User.java
@@ -13,9 +13,6 @@ import java.util.UUID;
 
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -24,4 +21,55 @@ public class User {
     private String email;
     private String password;
     private String profilePic;
+
+    public User(String email, UUID id, String username, String password, String profilePic) {
+        this.email = email;
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.profilePic = profilePic;
+    }
+
+    public User() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(String profilePic) {
+        this.profilePic = profilePic;
+    }
 }
