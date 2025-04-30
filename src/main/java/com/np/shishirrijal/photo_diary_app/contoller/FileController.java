f73086f2bb7e333de334d2d431834e529423f482
Shishir Rijal <ccrrizal0892@gmail.com>
2025-04-30 22:20:43 +0545

    Add cloud file update feature with azure blob

--- src/main/java/com/np/shishirrijal/photo_diary_app/contoller/FileController.java
+++ src/main/java/com/np/shishirrijal/photo_diary_app/contoller/FileController.java
@@ -1,8 +1,8 @@
 package com.np.shishirrijal.photo_diary_app.contoller;
 
-import com.np.shishirrijal.photo_diary_app.service.FileStorageService;
-import lombok.RequiredArgsConstructor;
+import com.np.shishirrijal.photo_diary_app.service.AzureBlobStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
  import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,22 +15,40 @@ import java.util.Map;
 
 
 @RestController
-@RequestMapping("/api/files/")
+@RequestMapping("/api/files")
 public class FileController {
 
-     private final FileStorageService fileStorageService;
+    @Autowired
+    private AzureBlobStorageService azureBlobStorageService;
 
-    public FileController(FileStorageService fileStorageService) {
-        this.fileStorageService = fileStorageService;
-    }
 
     @PostMapping("/upload")
-    public ResponseEntity<Map<String, String>> uploadPhoto(@RequestParam("photo") MultipartFile photo) {
-        String imageUrl = fileStorageService.saveFile(photo);
+    public ResponseEntity<Map<String, String>> uploadFile(@RequestParam("photo") MultipartFile photo) {
         Map<String, String> response = new HashMap<>();
-        response.put("imageUrl", imageUrl);
-        return ResponseEntity.ok(response);
+        try {
+            if (photo == null || photo.isEmpty()) {
+                response.put("error", "File is empty.");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+            }
+
+            // Validate file type
+            String mimeType = photo.getContentType();
+            if (!"image/jpeg".equals(mimeType) && !"image/png".equals(mimeType)) {
+                response.put("error", "Invalid file type. Only JPEG and PNG files are allowed.");
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+            }
+
+            String imageUrl = azureBlobStorageService.uploadFile(photo);
+            response.put("imageUrl", imageUrl);
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            // Log the exception for debugging
+            System.out.println("Error uploading file" + e);
+            response.put("error", "File upload failed due to an internal error.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
     }
+
 }
 
 
