da7fd142f99200011480c8357f58e67e3da80a7e
Shishir Rijal <ccrrizal0892@gmail.com>
2025-04-30 13:50:13 +0545

    add custom authentication provider

--- src/main/java/com/np/shishirrijal/photo_diary_app/config/SecurityConfig.java
+++ src/main/java/com/np/shishirrijal/photo_diary_app/config/SecurityConfig.java
@@ -1,16 +1,34 @@
 package com.np.shishirrijal.photo_diary_app.config;
 
+import com.np.shishirrijal.photo_diary_app.service.MyUserDetailsService;
+//import com.np.shishirrijal.photo_diary_app.util.JwtRequestFilter;
+import com.np.shishirrijal.photo_diary_app.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
- import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.*;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.*;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
- import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.*;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+//    @Autowired
+//    private JwtRequestFilter jwtRequestFilter;
+//
+    @Autowired
+    private MyUserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -23,7 +41,27 @@ public class SecurityConfig {
 //                .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                  .build();
+//                .userDetailsService(userDetailsService)
+//                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//        provider.setPasswordEncoder();
+
+        provider.setUserDetailsService(userDetailsService);
+        return provider;
+    }
+//
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
 }
--- src/main/java/com/np/shishirrijal/photo_diary_app/service/MyUserDetailsService.java
+++ src/main/java/com/np/shishirrijal/photo_diary_app/service/MyUserDetailsService.java
@@ -1,4 +1,29 @@
 package com.np.shishirrijal.photo_diary_app.service;
 
-public class MyUserDetailsService {
+import com.np.shishirrijal.photo_diary_app.model.PrincipalUser;
+import com.np.shishirrijal.photo_diary_app.model.User;
+import com.np.shishirrijal.photo_diary_app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+
+        if(user == null) {
+            System.out.println("Error: User not found with provided email");
+           throw new UsernameNotFoundException("User with specified username not found!");
+        }
+
+        return new PrincipalUser(user);
+    }
 }
